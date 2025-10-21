b79ec2bfdcecd6c92b01e23aac76494eff007f1c
naira124124124 <zenapazaro93@gmail.com>
2025-10-21 13:30:53 +0400

    Made decompose

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.models import Book
+
+book1 = Book("Гарри Поттер и Философский камень", "Дж.К.Роулинг", 1997, "978-5-17-080077-7")		
+print(book1.get_info())
--- src/models/book.py
+++ src/models/book.py
@@ -1,4 +1,4 @@
-class book:
+class Book:
 	def __init__(self, title: str, author: str, year: int, isbn:str):
 		self.title = title
 		self.author = author
@@ -8,14 +8,13 @@ class book:
 	def get_info(self) -> str:
 	    return f"{self.title}, автор - {self.author}, год издания - {self.year}"
 		
-class EBook(book):
+class EBook(Book):
 	def __init__(self, title: str, author: str, year, format: str, isbn:str):
 		super().__init__(title,author,year,isbn)
 		self.format = format
 	def get_info(self) -> str:
 		return f"{self.title}, автор - {self.author}, год издания - {self.year}, формат - {self.format}"
-book1 = book("Гарри Поттер и Философский камень", "Дж.К.Роулинг", 1997, "978-5-17-080077-7")		
-print(book1.get_info())	
 
-ebook1 = EBook("test_title","test_author",1997,"pdf","978-5-17-080077-7")
-print(ebook1.get_info())		
+
+#ebook1 = EBook("test_title","test_author",1997,"pdf","978-5-17-080077-7")
+#print(ebook1.get_info())		
